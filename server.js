2290c6a18a6481cbc6b3d9761b2bd90e9e40361f
israakittaneh <israakittanehm@gmail.com>
2020-11-05 11:15:51 +0200

    by work..no

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
